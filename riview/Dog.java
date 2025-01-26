878b325d7c3b5462a6d4c80332a1fac0d4d2e571
MD. Mohiuddin Rafi <87323620+mohiuddinrafi123@users.noreply.github.com>
2025-01-26 19:35:27 +0600

    Add files via upload

---
+++ riview/Dog.java
@@ -0,0 +1,9 @@
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void makeSound() { 
+        System.out.println(getName() + " says: gew!");
+    }
+}
