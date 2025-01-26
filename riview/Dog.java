e9a6020e245eeea77d39e0716974e5121cfc8301
MD. Mohiuddin Rafi <87323620+mohiuddinrafi123@users.noreply.github.com>
2025-01-26 19:55:49 +0600

    Delete riview directory

--- riview/Dog.java
+++
@@ -1,9 +0,0 @@
-class Dog extends Animal {
-    public Dog(String name) {
-        super(name);
-    }
-
-    public void makeSound() { 
-        System.out.println(getName() + " says: gew!");
-    }
-}
